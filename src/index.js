d13c56b6d4ae0f81063cb6ea335974de21b9a32a
Marcos Kubaszewski <marcosarobed@gmail.com>
2021-07-09 19:38:07 -0300

    Listando extrato

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
  */
 app.post('/account', (request, response) => {
     const { cpf,name } = request.body;
-
+    //retorna se exist/no exist
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf
     );
@@ -31,6 +31,15 @@ app.post('/account', (request, response) => {
         statement: []
     });
     return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+    const { cpf }  = request.params;
+    const customer = customers.find(customer => customer.cpf == cpf);
+    if(customer === undefined){
+        return response.status(404).json({error: "Customer not exists!"});
+    }
+    return response.json(customer.statement);
 });
 
 //localhost:3000
